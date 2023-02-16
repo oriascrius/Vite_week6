0c437b4b628fce8eff97863cf3c9e40cca49652f
oriascrius <77599353+oriascrius@users.noreply.github.com>
2023-02-17 07:41:39 +0800

    20230217

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <h1>你現在在前台頁面</h1>
     <div id="nav">
       <router-link to="/">首頁</router-link> |
-      <router-link to="/products">前台產品列表</router-link> |
+      <router-link to="/products">前台商品列表</router-link> |
       <router-link to="/cart">前台購物車</router-link> |
       <router-link to="/admin/products">後台購物車列表</router-link>
     </div>
