f4e5d04ddbdbdb9aab3f720179e189a75c542bad
oriascrius <qoq77416416@gmail.com>
2023-03-10 17:37:56 +0800

    20230310

--- src/components/front-end/AboutHeader.vue
+++ src/components/front-end/AboutHeader.vue
@@ -4,7 +4,7 @@
     style="
       width: 100%;
       height: 768px;
-      background-image: url('/images/AboutUs_Banner_001.png');
+      background-image: url(./images/AboutUs_Banner_001.png);
       background-repeat: no-repeat;
       background-position: top;
       background-size: cover;
--- src/components/front-end/ContactHeader.vue
+++ src/components/front-end/ContactHeader.vue
@@ -4,7 +4,7 @@
     style="
       width: 100%;
       height: 768px;
-      background-image: url('/images/Contact_header.png');
+      background-image: url(./images/Contact_header.png);
       background-repeat: no-repeat;
       background-position: top;
       background-size: cover;
